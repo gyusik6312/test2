783195a370bf82d0a7960f097b9fa8bde92363c6
daeseong0316 <dsh1345@naver.com>
2026-04-09 03:34:57 +0900

    헤헤헤

--- team-project/src/Time.jsx
+++ team-project/src/Time.jsx
@@ -3,16 +3,47 @@ import { useDebugValue, useState } from "react"
 function Time({name, age})
 {
     const [text, setText] = useState("");
+    const [count, setCount] = useState("");
+    const [backtest, setwetl] = useState("");
+    const [log, settlxzk] = useState("");
+    const [value, setValue] = useState("");
+    const [serch, setSearch] = useState("");
+    const [sadfkrk, setdljwe] = useState("");
+    const [box, setBox] = useState("");
+    const [test, setTest] = useState("");
+
+    let number = 0;
+    const num = true;
 
     function reset()
     {
         setText("");
     }
 
-    function handleOnChange(e)
+    function handlesetValue(e)
     {
-        setText(e.target.value)
+        setValue(e.target.value)
     }
+
+    function handlesettlxzk(e)
+    {
+        settlxzk(e.target.value)
+    }
+
+    function handlesetBox(e)
+    {
+        setBox(e.target.value)
+    }
+
+    function handlesetwetl(e)
+    {
+        setwetl(e.target.value)
+    }
+
+    function handleOnChange(e) {
+    setText(e.target.value);
+    }
+    
 
     return (
         <div>
@@ -20,6 +51,14 @@ function Time({name, age})
             <h3>제 나이는 {age}입니다.</h3>
             <input value = {text} onChange = {handleOnChange}/>
             <button onClick={reset}>초기화</button>
+            <input value = {text} onChange = {handleOnChange}/>
+            <button onClick={reset}>초기화</button>
+            <input value = {text} onChange = {handleOnChange}/>
+            <button onClick={reset}>초기화</button>
+            <h3>제 나이는 {age}입니다.</h3>
+            <input value = {text} onChange = {handleOnChange}/>
+            <button onClick={reset}>초기화</button>
+            <h3>제 성별은 {age}입니다.</h3>
         </div>
     )
 }
